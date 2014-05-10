6492ddee194c699027438898b07e2d8a7ebf704a
Andrew Rivett <veggiefrog@gmail.com>
2014-05-10 09:30:54 -0400

    Removed duplicate errors

--- public/albumAdd.php
+++ public/albumAdd.php
@@ -57,7 +57,7 @@ function ciniki_gallery_albumAdd(&$ciniki) {
 		return $rc;
 	}
 	if( $rc['num_rows'] > 0 ) {
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1706', 'msg'=>'You already have an album with this name, please choose another name'));
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1709', 'msg'=>'You already have an album with this name, please choose another name'));
 	}
 
 	//
