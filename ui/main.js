6991d177e9cb26449b93a79d32ee941d99a4cd6d
Andrew Rivett <veggiefrog@gmail.com>
2015-11-02 21:58:34 -0500

    Updated UI for image field

--- ui/main.js
+++ ui/main.js
@@ -121,7 +121,7 @@ function ciniki_gallery_main() {
 		this.edit.default_data = {};
 		this.edit.data = {};
 		this.edit.sections = {
-			'_image':{'label':'Photo', 'fields':{
+			'_image':{'label':'Photo', 'type':'imageform', 'fields':{
 				'image_id':{'label':'', 'type':'image_id', 'hidelabel':'yes', 'controls':'all', 'history':'no'},
 			}},
 			'info':{'label':'Information', 'type':'simpleform', 'fields':{
