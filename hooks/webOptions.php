b7caf0a4db7b81232cc05ffef1ca5459464e2434
Andrew Rivett <veggiefrog@gmail.com>
2016-10-02 20:39:12 -0400

    Added ability to hook web pages into gallery

---
+++ hooks/webOptions.php
@@ -0,0 +1,56 @@
+<?php
+//
+// Description
+// -----------
+// This function will return the list of options for the module that can be set for the website.
+//
+// Arguments
+// ---------
+// ciniki:
+// settings:        The web settings structure.
+// business_id:     The ID of the business to get options for.
+//
+// args:            The possible arguments for profiles
+//
+//
+// Returns
+// -------
+//
+function ciniki_gallery_hooks_webOptions(&$ciniki, $business_id, $args) {
+
+    //
+    // Check to make sure the module is enabled
+    //
+    if( !isset($ciniki['business']['modules']['ciniki.gallery']) ) {
+        return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'3648', 'msg'=>"I'm sorry, the page you requested does not exist."));
+    }
+
+    //
+    // Get the settings from the database
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbDetailsQueryDash');
+    $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_web_settings', 'business_id', $business_id, 'ciniki.web', 'settings', 'page-gallery');
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    if( !isset($rc['settings']) ) {
+        $settings = array();
+    } else {
+        $settings = $rc['settings'];
+    }
+
+    $pages['ciniki.gallery'] = array('name'=>'Gallery', 'options'=>array(
+/*        array('label'=>'Sidebar',
+            'setting'=>'page-artistprofiles-sidebar', 
+            'type'=>'toggle',
+            'value'=>(isset($settings['page-artistprofiles-sidebar'])?$settings['page-artistprofiles-sidebar']:'no'),
+            'toggles'=>array(
+                array('value'=>'no', 'label'=>'No'),
+                array('value'=>'yes', 'label'=>'Yes'),
+                ),
+            ), */
+        ));
+
+    return array('stat'=>'ok', 'pages'=>$pages);
+}
+?>
---
+++ web/processRequest.php
@@ -0,0 +1,255 @@
+<?php
+//
+// Description
+// -----------
+// This function will process a web request for the blog module.
+//
+// Arguments
+// ---------
+// ciniki:
+// settings:        The web settings structure.
+// business_id:     The ID of the business to get post for.
+//
+// args:            The possible arguments for posts
+//
+//
+// Returns
+// -------
+//
+function ciniki_gallery_web_processRequest(&$ciniki, $settings, $business_id, $args) {
+
+    if( !isset($ciniki['business']['modules']['ciniki.gallery']) ) {
+        return array('stat'=>'404', 'err'=>array('pkg'=>'ciniki', 'code'=>'3643', 'msg'=>"I'm sorry, the page you requested does not exist."));
+    }
+    $page = array(
+        'title'=>$args['page_title'],
+        'breadcrumbs'=>$args['breadcrumbs'],
+        'blocks'=>array(),
+        );
+
+    //
+    // Setup titles
+    //
+    if( count($page['breadcrumbs']) == 0 ) {
+        $page['breadcrumbs'][] = array('name'=>'Gallery', 'url'=>$args['base_url']);
+    }
+
+    //
+    // Default to display list of albums
+    //
+    $display = 'albums';
+    $ciniki['response']['head']['og']['url'] = $args['domain_base_url'];
+
+    //
+    // Setup the base url as the base url for this page. This may be altered below
+    // as the uri_split is processed, but we do not want to alter the original passed in.
+    //
+
+    //
+    // Parse the url
+    //
+    $uri_split = $args['uri_split'];
+
+    //
+    // Get the list of albums
+    //
+    $strsql = "SELECT ciniki_gallery_albums.id, "
+        . "ciniki_gallery_albums.name AS title, "
+        . "ciniki_gallery_albums.permalink, "
+        . "ciniki_gallery_albums.description, "
+        . "COUNT(ciniki_gallery.album_id) AS num_images "
+        . "FROM ciniki_gallery_albums "
+        . "LEFT JOIN ciniki_gallery ON ("
+            . "ciniki_gallery_albums.id = ciniki_gallery.album_id "
+            . "AND ciniki_gallery.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+            . "AND (ciniki_gallery.webflags&0x01) = 0 "
+            . ") "
+        . "WHERE ciniki_gallery_albums.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+        . "AND (ciniki_gallery_albums.webflags&0x01) = 0 "
+        . "AND ciniki_gallery_albums.name <> '' "
+        . "GROUP BY ciniki_gallery_albums.id "
+        . "HAVING num_images > 0 "
+        . "";
+    if( !isset($settings['page-gallery-album-sort']) 
+        || $settings['page-gallery-album-sort'] == 'name-asc' ) {
+        $strsql .= "ORDER BY ciniki_gallery_albums.name ";
+    } elseif( $settings['page-gallery-album-sort'] == 'name-desc' ) {
+        $strsql .= "ORDER BY ciniki_gallery_albums.name DESC ";
+    } elseif( $settings['page-gallery-album-sort'] == 'sequence-asc' ) {
+        $strsql .= "ORDER BY ciniki_gallery_albums.sequence ASC, ciniki_gallery_albums.name ";
+    } elseif( $settings['page-gallery-album-sort'] == 'sequence-desc' ) {
+        $strsql .= "ORDER BY ciniki_gallery_albums.sequence DESC, ciniki_gallery_albums.name ";
+    } elseif( $settings['page-gallery-album-sort'] == 'startdate-desc' ) {
+        $strsql .= "ORDER BY ciniki_gallery_albums.start_date DESC, ciniki_gallery_albums.name ";
+    } elseif( $settings['page-gallery-album-sort'] == 'startdate-desc' ) {
+        $strsql .= "ORDER BY ciniki_gallery_albums.start_date DESC, ciniki_gallery_albums.name ";
+    }
+        
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryArrayTree');
+    $rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.gallery', array(
+        array('container'=>'albums', 'fname'=>'permalink', 
+            'fields'=>array('id', 'title', 'permalink', 'description', 'num_images')),
+        ));
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    if( !isset($rc['albums']) ) {
+        $page['blocks'][] = array('type'=>'content', 'content'=>"There are currently no photos available. Please check back soon.");
+        return array('stat'=>'ok', 'page'=>$page);
+    }
+    $albums = $rc['albums'];
+
+    //
+    // Check if album specified
+    //
+    $cur_album = null;
+    if( isset($uri_split[0]) && $uri_split[0] != '' ) {
+        foreach($albums as $album) {
+            if( $album['permalink'] == $uri_split[0] ) {
+                $cur_album = $album;
+                break;
+            }
+        }
+        if( $cur_album == null ) {
+            return array('stat'=>'404', 'err'=>array('pkg'=>'ciniki', 'code'=>'3644', 'msg'=>'Album not found'));
+        }
+    }
+
+    //
+    // Check if only one album
+    //
+    if( count($albums) == 1 ) {
+        $cur_album = array_pop($albums);
+    }
+
+    //
+    // Load the list of images for the album
+    //
+    if( $cur_album != null ) {
+        $display = 'album';
+        $page['breadcrumbs'][] = array('name'=>$cur_album['title'], 'url'=>$args['base_url'] . '/' . $cur_album['permalink']);
+
+        $strsql = "SELECT ciniki_gallery.id, "
+            . "ciniki_gallery.name AS title, "
+            . "ciniki_gallery.permalink, "
+            . "ciniki_gallery.image_id, "
+            . "ciniki_gallery.description, "
+            . "IF(ciniki_images.last_updated > ciniki_gallery.last_updated, "
+                . "UNIX_TIMESTAMP(ciniki_images.last_updated), "
+                . "UNIX_TIMESTAMP(ciniki_gallery.last_updated)) AS last_updated "
+            . "FROM ciniki_gallery "
+            . "LEFT JOIN ciniki_images ON ("
+                . "ciniki_gallery.image_id = ciniki_images.id "
+                . "AND ciniki_images.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+                . ") "
+            . "WHERE ciniki_gallery.album_id = '" . ciniki_core_dbQuote($ciniki, $cur_album['id']) . "' "
+            . "AND ciniki_gallery.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+            . "AND ciniki_gallery.image_id > 0 "
+            . "AND (ciniki_gallery.webflags&0x01) = 0 "
+            . "ORDER BY ciniki_gallery.date_added DESC ";
+
+        $rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.gallery', array(
+            array('container'=>'images', 'fname'=>'id', 'fields'=>array('id', 'title', 'permalink', 'image_id', 'description')),
+            ));
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+        if( !isset($rc['images']) || count($rc['images']) == 0 ) {
+            return array('stat'=>'404', 'err'=>array('pkg'=>'ciniki', 'code'=>'3645', 'msg'=>'No images found.'));
+        }
+        $album_images = $rc['images'];
+    }
+
+    //
+    // Check if image specified
+    //
+    $first_image = null;
+    $prev_image = null;
+    $cur_image = null;
+    $next_image = null;
+    $last_image = null;
+    if( isset($uri_split[1]) && $uri_split[1] != '' ) {
+        //
+        // Find the image
+        //
+        foreach($album_images as $image) {
+            if( $first_image == null ) {
+                $first_image = $image;
+            }
+            if( $image['permalink'] == $uri_split[1] ) {
+                $cur_image = $image;
+            } elseif( $cur_image != null && $next_image == null ) {
+                $next_image = $image;
+            }
+            if( $cur_image == null ) {
+                $prev_image = $image;
+            }
+            $last_image = $image;
+        }
+        if( $cur_image == null ) {
+            return array('stat'=>'404', 'err'=>array('pkg'=>'ciniki', 'code'=>'3646', 'msg'=>"I'm sorry, but we couldn't find the image your requested."));
+        }
+        if( count($album_images) == 1 ) {
+            $prev_image = null;
+            $next_image = null;
+        } elseif( $prev_image == null ) {
+            $prev_image = $last_image;
+        } elseif( $next_image == null ) {
+            $next_image = $first_image;
+        }
+        $display = 'image';
+        $page['breadcrumbs'][] = array('name'=>$cur_image['title'], 'url'=>$args['base_url'] . '/' . $cur_album['permalink'] . '/' . $cur_image['permalink']);
+    }
+
+
+    //
+    // Display the list of albums
+    //
+    if( $display == 'albums' ) {
+        foreach($albums as $aid => $album) {
+            //
+            // Look for the highlight image, or the most recently added image
+            //
+            $strsql = "SELECT ciniki_gallery.image_id, ciniki_images.image "
+                . "FROM ciniki_gallery, ciniki_images "
+                . "WHERE ciniki_gallery.business_id = '" . ciniki_core_dbQuote($ciniki, $business_id) . "' "
+                . "AND album_id = '" . ciniki_core_dbQuote($ciniki, $album['id']) . "' "
+                . "AND ciniki_gallery.image_id = ciniki_images.id "
+                . "AND (ciniki_gallery.webflags&0x01) = 0 "
+                . "ORDER BY (ciniki_gallery.webflags&0x10) DESC, ciniki_gallery.date_added DESC "
+                . "LIMIT 1";
+            $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.gallery', 'image');
+            if( $rc['stat'] != 'ok' ) {
+                return $rc;
+            }
+            if( isset($rc['image']) ) {
+                $albums[$aid]['image_id'] = $rc['image']['image_id'];
+            }
+        }
+        $page['blocks'][] = array('type'=>'tagimages', 'base_url'=>$args['base_url'], 'tags'=>$albums);
+    } 
+   
+    //
+    // Display the list of thumbnails for an album
+    //
+    elseif( $display == 'album' ) {
+        $page['blocks'][] = array('type'=>'gallery', 'base_url'=>$args['base_url'] . '/' . $cur_album['permalink'], 'images'=>$album_images);
+    } 
+    
+    //
+    // Display an image from an album
+    //
+    elseif( $display == 'image' ) {
+        $block = array('type'=>'galleryimage', 'image'=>$cur_image);
+        if( $prev_image != null ) {
+            $block['prev'] = array('url'=>$args['base_url'] . '/' . $cur_album['permalink'] . '/' . $prev_image['permalink']);
+        }
+        if( $next_image != null ) {
+            $block['next'] = array('url'=>$args['base_url'] . '/' . $cur_album['permalink'] . '/' . $next_image['permalink']);
+        }
+        $page['blocks'][] = $block;
+    }
+    
+    return array('stat'=>'ok', 'page'=>$page);
+}
+?>
